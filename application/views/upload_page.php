9364c7f8ad259f990e50d8cc7af06577ca74f00c
NY <mepo.opem0211@gmail.com>
2020-12-24 16:11:35 +0900

    #9 ひな形仮制作

---
+++ application/views/upload_page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <link rel="stylesheet">
+    <?php echo link_tag("/assets/kinsta_css/upload_style.css"); ?>
+</head>
+<body>
+    <main>
+        <ul>
+            <li class="drop"><input type="text" placeholder="ここに画像をドロップ"></li>
+            <li class="or">または</li>
+            <li class="in_url"><input type="text" placeholder="URLを入力"></li>
+            <li class="message">メッセージ</li>
+            <li class="in_message"><input type="text"></li>
+            <li class="hashtag_key">ハッシュタグキーワード</li>
+            <li class="in_hashtag_key"><input type="text"></li>
+            <li class="my_traning">マイトレーニング</li>
+            <li class="in_my_traning"><input type="text"></li>
+            <li class="upload_button"><button type="submit">送信</button></li>
+        </ul>
+    </main>
+</body>
+</html>
