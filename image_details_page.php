0ef38dbaaed24c0c97fa7dd10eeccd56a1602492
NY <mepo.opem0211@gmail.com>
2020-12-26 16:57:28 +0900

    firstcommit

---
+++ image_details_page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <link rel="stylesheet">
+    <?php echo link_tag("/assets/kinsta_css/image_details_style.css"); ?>
+</head>
+<body>
+    <main>
+        <ul>
+            <li class="icon">アイコン</li>
+            <li class="name_no1">タンクトッパー</li>
+            <li class="mussule_member">マッスルメンバー登録</li>
+            <li class="picture">picture1</li>
+            <li class="icon">アイコン</li>
+            <li class="name_no1">タンクトッパー</li>
+            <li class="mussule_member">マッスルメンバー登録</li>
+            <li class="message">メッセージ</li>
+            <li class="mune">胸キレボタン</li>
+            <li class="ude">腕キレボタン</li>
+            <li class="kata">肩キレボタン</li>
+            <li class="hara">腹キレボタン</li>
+            <li class="ashi">足キレボタン</li>
+            <li class="kiretemasu">キレてますボタン+100</li>
+            <li class="traning">マイトレーニング</li>
+            <li class="traning_details">腕立て</li>
+            <li class="traning_details">腕立て</li>
+            <li class="traning_details">・・・</li>
+        </ul>
+    </main>
+
+</body>
+</html>
