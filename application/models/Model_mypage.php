04d9153bd55e98a361aaa75332fd82ee77e42da4
yutaro <yutaro.fujita.tech.0808@gmail.com>
2021-03-09 22:39:56 +0900

    #3mypageを更新

--- application/models/Model_mypage.php
+++ application/models/Model_mypage.php
@@ -17,7 +17,7 @@ class Model_mypage extends CI_Model
     public function mypage_get()
     {
          //自らのプロフィール情報を取り出す ※ 自分判定
-        $this->db->where('user_id',1);
+        $this->db->where('user_id',48);
          //dbのusersテーブルから取得
         $query = $this->db->get('users');
          //配列に入れる
@@ -32,7 +32,7 @@ class Model_mypage extends CI_Model
     public function individual_get()
     {
         //自らの投稿の情報を取り出す　※　自分の投稿判定
-        $this->db->where('post_id',1);
+        $this->db->where('post_id', 48);
         //dbのpostsテーブルから取得
         $query = $this->db->get('posts');
         //配列に入れる
